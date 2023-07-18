c559a0538093a03d3de3b2014d8258650344667e
Ubuntu <ubuntu@ip-172-31-28-133.ap-northeast-1.compute.internal>
2023-07-18 08:08:48 +0000

    QuizApp完成

--- main.js
+++ main.js
@@ -1,6 +1,112 @@
-import 'whatwg-fetch';
+const startQuizButton = document.getElementById('start-quiz-button');
+let messageSubject = document.getElementById('message-subject');
+let messageContent = document.getElementById('message-content');
+let quizContentWrapper = document.getElementById('quiz-content-wrapper');
+let buttonWrapper = document.getElementById('button-wrapper');
+let correctNumber = 0;
+let quizNumber = 0;
 
-fetch('https://opentdb.com/api.php?amount=10&type=multiple')
-  .then((response) => response.text())
-  .then((text) => console.log(text))
-  .catch((error) => console.log(error));
+
+
+startQuizButton.addEventListener('click', function (event) {
+    messageSubject.textContent = "取得中";
+    messageContent.textContent = "少々お待ちください";
+    fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+        .then((response) => response.text())
+        .then((text) => {
+            console.log(text)
+            const fetchText = JSON.parse(text);
+            console.log(`配列数は${fetchText.results.length}`);
+            correctNumber = 0;
+            console.log(correctNumber);
+            quizNumber = 0;
+            console.log(quizNumber);
+            newQuestion(quizNumber, fetchText);
+
+        })
+        .catch((error) => console.log(error));
+});
+
+
+// シャッフルされる配列
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// クリックイベントのハンドラ関数
+function handleClick(event) {
+    // クリックされた要素のIDを表示する
+    console.log(event.target.id);
+}
+
+function newQuestion(quizNumber, fetchText) {
+    if (quizNumber < 10) {
+        console.log('問題番号は' + quizNumber);
+        let displayQuizContentHTML = ''
+        displayQuizContentHTML = `
+        <h3>[ジャンル]${fetchText.results[quizNumber].category}</h3>
+        <h3>[難易度]${fetchText.results[quizNumber].difficulty}</h3>
+        `;
+        quizContentWrapper.innerHTML = displayQuizContentHTML;
+        messageContent.textContent = fetchText.results[quizNumber].question;
+        startQuizButton.style.display = 'none';
+        const correctAnswer = fetchText.results[quizNumber].correct_answer;
+        const incorrectAnswers = fetchText.results[quizNumber].incorrect_answers;
+        const quizSelect = shuffleArray([correctAnswer, ...incorrectAnswers]);
+        console.log(quizSelect);
+        let displayButtonWrapperHTML = ''
+        displayButtonWrapperHTML = `
+        <button id="question${quizNumber}-answer-button-1" value="${quizSelect[0]}">${quizSelect[0]}</button><br>
+        <button id="question${quizNumber}-answer-button-2" value="${quizSelect[1]}">${quizSelect[1]}</button><br>
+        <button id="question${quizNumber}-answer-button-3" value="${quizSelect[2]}">${quizSelect[2]}</button><br>
+        <button id="question${quizNumber}-answer-button-4" value="${quizSelect[3]}">${quizSelect[3]}</button>
+        `;
+        buttonWrapper.innerHTML = displayButtonWrapperHTML;
+        const buttonClick = document.querySelectorAll('[id*="answer-button"]');
+        buttonClickEvent(buttonClick, correctAnswer, fetchText);
+    } else {
+        console.log('正答数は' + correctNumber);
+        displayQuizContentHTML = ''
+        displayButtonWrapperHTML = `
+        <button id="go-to-home-button" value="ホームに戻る">ホームに戻る</button>
+        `
+        quizContentWrapper.innerHTML = displayQuizContentHTML;
+        buttonWrapper.innerHTML = displayButtonWrapperHTML;
+        const goToHomeButton = document.getElementById('go-to-home-button');
+        goToHomeButton.addEventListener('click', function (event) {
+            displayQuizContentHTML = ''
+            displayButtonWrapperHTML = ''
+            quizContentWrapper.innerHTML = displayQuizContentHTML;
+            buttonWrapper.innerHTML = displayButtonWrapperHTML;
+            messageSubject.textContent = "ようこそ";
+            messageContent.textContent = "以下のボタンをクリック";
+            startQuizButton.style.display = 'inline';
+        });
+        messageSubject.textContent = "あなたの正答数は" + correctNumber + "です";
+        messageContent.textContent = "再度チャレンジしたい場合は以下をクリック";
+
+    }
+}
+function buttonClickEvent(buttonClick, correctAnswer, fetchText) {
+    quizNumber += 1;
+    messageSubject.textContent = '問題' + quizNumber;
+    buttonClick.forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            const buttonClickValue = event.target.value;
+            console.log(buttonClickValue);
+            if (buttonClickValue === correctAnswer) {
+                console.log('正解です');
+                correctNumber += 1;
+                console.log('正解数は' + correctNumber);
+                newQuestion(quizNumber, fetchText);
+            } else {
+                console.log('不正解です');
+                newQuestion(quizNumber, fetchText);
+            }
+        });
+    });
+}
